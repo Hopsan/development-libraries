a93e3242f0d085f79edc2c9ea151a2efe5045121
Petter Krus <petter.krus@gmail.com>
2021-11-11 11:29:03 +0100

    ActivityDiagramLib folder reaorganissed

--- ActivityDiagrams/ActivityDiagram/compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=C:/Program Files/Hopsan214/mingw64/bin;%PATH%
-@echo on
-if exist ActivityDiagramLibrary.dll (
-  del ActivityDiagramLibrary.dll
-)
-g++ -I"C:/Program Files/Hopsan214/HopsanCore/include" -fPIC -w -Wl,--rpath,"G:/Git/development-libraries/ActivityDiagrams/ActivityDiagram" -DHOPSAN_BUILD_TYPE_RELEASE -std=c++11  ActivityDiagramLib.cc -L"C:/Program Files/Hopsan214/bin" -L"C:/Program Files/Hopsan214/lib" -lhopsancore  -shared -o ActivityDiagramLibrary.dll
