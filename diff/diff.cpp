bfe69c56c98a06418d696036bcf1df1ee9307fbc
lselter <Lselter@kawasakihydraulics.com>
2022-10-11 13:04:07 +0100

    Addresses PR Comments
    In/out Variable renamed to `in` and `out`.
    Empty methods removed
    Variable `prev` changed from member to private variable
    Blocks renamed to `SignalDifference` & `SignalDifferentiator` respectively.
    Removes redundant includes.

--- diff/diff.cpp
+++ diff/diff.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "ComponentEssentials.h"
-#include "diffDiff.hpp"
-#include "diffDifft.hpp"
+#include "SignalDifference.hpp"
+#include "SignalDifferentiator.hpp"
 
 using namespace hopsan;
 
 extern "C" DLLEXPORT void register_contents(ComponentFactory* pComponentFactory, NodeFactory* pNodeFactory)
 {
     //Register Components
-    pComponentFactory->registerCreatorFunction("diffDiff", diffDiff::Creator);
-    pComponentFactory->registerCreatorFunction("diffDifft", diffDifft::Creator);
+    pComponentFactory->registerCreatorFunction("SignalDifference", SignalDifference::Creator);
+    pComponentFactory->registerCreatorFunction("SignalDifferentiator", SignalDifferentiator::Creator);
 
     //Register custom nodes (if any)
     HOPSAN_UNUSED(pNodeFactory);
